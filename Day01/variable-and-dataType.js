8622e8e0ef94a94b1fe04bbe25f6d3f81313754e
Rana Pratap Singh Patel <ranapsp317@gmail.com>
2024-07-13 14:31:55 +0530

    Day 1 commit

---
+++ Day01/variable-and-dataType.js
@@ -0,0 +1,59 @@
+//Activity 01: Variable Declaration
+
+//Task-01: Declare a variable using var, assign it a number and log the value to the console.
+// var x=10;
+// console.log(x);
+
+//task-02:Declare a variable using let, assign it a string  and log the value to the console. 
+// let s="rana"
+// console.log(s);
+
+//Activity-02 :Constant Declaration
+
+//Task-03 : Declare a variable using const, assign it a boolean value and log the value to the console.
+// const x=true;
+// console.log(x);
+
+//Activity-03 :Data Type
+
+//Task-04: create variables of different Data types(number, string, boolen, object, array) and log each variable's
+//         types using typeof operator.
+
+/* let n=10;
+let s="rana";
+let rana={
+    name:"Rana Pratap Singh Patel",
+    age:22,
+    height:5.5
+};
+let x=false;
+let y=[1,2,3,4,5];
+
+console.log(typeof(n));
+console.log(typeof(s));
+console.log(typeof(rana));
+console.log(typeof(x));
+console.log(typeof(y));  // typeof(y) will treat array as an object due to its compatibility reasons. 
+//more reliable methode to check for an array in js is-
+console.log(Array.isArray(y));
+
+*/
+
+/*  Activity-04: Reassigning Variables
+Task-05: Declare a variable using let, assign it an initial value, reassign it a new value and log both values to the console
+let x=5;
+console.log(x);
+
+x=6;
+console.log(x);
+*/
+
+/*Activity-05: Understanding Const
+  task-06: try reassig a variable declare with const and observe the error 
+   const x=10;
+   x=5;
+   console.log(x); // the error show assignment to a constant variable.
+*/
+
+
+
