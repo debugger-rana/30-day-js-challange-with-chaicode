10b3149ded90120356af5a9ff20706df81bb2c8e
Rana Pratap Singh Patel <ranapsp317@gmail.com>
2024-07-17 08:23:01 +0530

    Day 3

---
+++ Day04/loop.js
@@ -0,0 +1,66 @@
+//task1: print (1-10)
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+//task02: multiplicatoin table of 5
+// for(let i=1;i<=10;i++){
+//     console.log(i*5);
+// }
+
+//task03:Sum of number from 1-10
+// let i=1; 
+// let sum=0;
+// while(i<=10){
+//     sum=i+sum;
+//     i++;
+// }
+// console.log(sum);
+
+//task04:print the numbe from 10-1 using while loop
+// let i=10;
+// while(i>1){
+//     console.log(i);
+//     i--;
+// }
+
+//task05:print the number from 1 to 5 using do while loop
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+
+//task06:calculate factorial of a number using  do while loop
+// let number=5;
+// let fact=1;
+// do{
+//     fact=fact*number;
+//     number--;
+// }while(number > 0);
+// console.log(fact)
+
+//task07:write a program to print pattern using nested loop
+// for(let i=1;i<=5;i++){
+//     let row=" ";
+//     for(j=1;j<=i;j++){
+//         row+="*";
+//     }
+//     console.log(row);
+// }
+
+
+// task08:program to print number from 1- 10 and skipp 5 using continue;
+// for(let i=1;i<=10;i++){
+//     if(i==5) continue;
+//     else
+//      console.log(i);
+// }
+
+//task09:print 1-10 when number is 7 just break the statement;
+for(let i=1;i<=10;i++){
+    if(i==7) break;
+    else console.log(i);
+}
+
+
